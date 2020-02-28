5954ddae1a6ece47535bfc216ba00ba959aac437
Tosun Sait <tosunsa@cronos.be>
2020-02-28 18:18:46 +0100

    Passing Ingredients from Recipes to the

--- src/app/shared/recipe.service.ts
+++ src/app/shared/recipe.service.ts
@@ -1,3 +1,4 @@
+import { ShoppingListService } from './shopping-list.service';
 import { Ingredient } from './ingredient.model';
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe/recipe.model';
@@ -18,21 +19,25 @@ export class RecipeService {
         new Ingredient('meat', 1),
         new Ingredient('french fries', 20)
       ]),
-    new Recipe(
-      'an another recipe',
-      'this is first one',
-      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
-      [
-        new Ingredient('buns', 2),
-        new Ingredient('meat', 1)
-      ])
-  ];
-
-  getRecipes() {
-    // tslint:disable-next-line:max-line-length
-    // If I were to return it like this, I actually return the direct reference to this array and since arrays and objects are reference types in Javascript, so has nothing to do with Angular, vanilla Javascript that is, well if we now change something on this array, we will change it on the array in this service. Therefore, I will call slice with no arguments, this will simply return a new array which is an exact copy of the one in this service file. So therefore, we really can't access the recipes array stored here from outside,we only get a copy, so that is a way to get our recipe.
+      new Recipe(
+        'an another recipe',
+        'this is first one',
+        'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+        [
+          new Ingredient('buns', 2),
+          new Ingredient('meat', 1)
+        ])
+      ];
+      constructor(private shoppingListService: ShoppingListService) { }
+      
+      getRecipes() {
+        // tslint:disable-next-line:max-line-length
+        // If I were to return it like this, I actually return the direct reference to this array and since arrays and objects are reference types in Javascript, so has nothing to do with Angular, vanilla Javascript that is, well if we now change something on this array, we will change it on the array in this service. Therefore, I will call slice with no arguments, this will simply return a new array which is an exact copy of the one in this service file. So therefore, we really can't access the recipes array stored here from outside,we only get a copy, so that is a way to get our recipe.
     return this.recipes.slice();
   }
 
-  constructor() { }
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
+  }
+
 }
