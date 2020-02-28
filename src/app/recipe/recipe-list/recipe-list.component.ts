4a384743ab7e3e6833fb76d3479d82829c56889d
Tosun Sait <tosunsa@cronos.be>
2020-02-28 15:21:45 +0100

    managing recipes in a recipe service

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { RecipeService } from './../../shared/recipe.service';
 import { Recipe } from './../recipe.model';
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
@@ -8,17 +9,15 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  // we will define the type for this property and the title will be recipe
-  recipes: Recipe[] = [
-    new Recipe('a test recipe', 'this is first one', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('an another recipe', 'this is first one', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
+    // we will get this copy of recipes, this copy of that array.
+    this.recipes = this.recipeService.getRecipes();
   }
- 
+
   onRecipeSelected(recipe: Recipe) {
     this.recipeWasSelected.emit(recipe);
   }
