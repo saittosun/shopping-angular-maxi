b27e82cf470839e141ed398f926f28477fa3f70b
Tosun Sait <tosunsa@cronos.be>
2020-02-28 17:12:24 +0100

    using services for pushing data from a to b

--- src/app/shared/shopping-list.service.ts
+++ src/app/shared/shopping-list.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import { Ingredient } from './ingredient.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient('apples', 5),
     new Ingredient('tomato', 10)
@@ -17,5 +18,6 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
   }
 }
